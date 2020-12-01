0befc931dde1e210f5566e93714fd6bc11876ede
Trung Tran <trungth140694@gmail.com>
2020-12-02 00:14:57 +0700

    Add extra function for ResourceViewSet with model extra field

--- finance_core_api/api.py
+++ finance_core_api/api.py
@@ -1,3 +1,8 @@
+from caching.jsondb import ModelSerializer
+from django.http import HttpResponse
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from finance_core_api.models import Account, Resource
 from rest_framework import viewsets, permissions
 from .serializers import AccountSerializer, ResourceSerializer
@@ -14,3 +19,9 @@ class ResourceViewSet(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
     permission_classes = []
+
+    @action(methods=['get'], detail=True, url_path="related-accounts")
+    def get_related_accounts(self, request, pk):
+        result = Account.objects.filter(fk_resource_id=pk)
+        accounts = AccountSerializer(result, many=True)
+        return Response(accounts.data)
--- finance_core_api/serializers.py
+++ finance_core_api/serializers.py
@@ -5,10 +5,18 @@ from finance_core_api.models import Account, Resource
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        exclude = ['id']
+        exclude = ['fk_resource']
 
 
 class ResourceSerializer(serializers.ModelSerializer):
+    total_accounts = serializers.SerializerMethodField('get_total_accounts')
+
     class Meta:
         model = Resource
-        exclude = ['id']
+        fields = "__all__"
+
+    @staticmethod
+    def get_total_accounts(instance):
+        account_query_set = Account.objects.filter(fk_resource_id=instance.id)
+        total_account = account_query_set.count()
+        return total_account
